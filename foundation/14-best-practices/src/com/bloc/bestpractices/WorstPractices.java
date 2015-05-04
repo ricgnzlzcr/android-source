73940d905da5c46e0105b08f748f9bbc798a99ea
Ricardo Gonzalez <ricgnzlzcr@gmail.com>
2015-05-04 17:33:50 -0400

    Checkpoint 14 completed

--- foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
+++ foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -12,29 +12,34 @@ public class WorstPractices extends Object {
 	/************************************************/
 
 	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
-	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
-	{
-	MAGIC_NUMBER--;
-	}
-	}
+		int mNum = WorstPractices.magicNumber(false);
+		mNum *= 5;
+		if (mNum > 18) {
+			while(mNum > 0) {
+				System.out.println(String.valueOf(mNum));
+				mNum--;
+			}
+		}
 	}
 
-// animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
-// returns: a magical number
-private static int animals(boolean yeswellheresthething)
-{
-		/*
-			Start off with one of these
-		*/
-	int aInt = yeswellheresthething ? 34 : 21;
-	float sparklesfairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
-	} return (int) sparklesfairy * aInt;
+
+	/*********************
+	* Function: animals
+	* Usage: int x = WorstPractices.magicNumber(true);
+	* Returns: integer representation of a Magic Number
+	* ------------------------------------------------
+	* Takes in a boolean that helps determine the magic number
+	* recieved by the user. 
+	/*********************/
+
+	private static int magicNumber(boolean getMagicNumber) {
+		
+		int aInt = getMagicNumber ? 34 : 21;
+		float multiplier = .5f;
+		for (int i = 0; i < 5; i++) { 
+			multiplier *= (float)aInt / 10;
+		} 
+		return (int) multiplier * aInt;
 }
 
 	/************************************************
